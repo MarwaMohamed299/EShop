ad807eb807823c9bb8ff42b762086dfaf17b1d0d
MarwaMohamed299 <128231159+MarwaMohamed299@users.noreply.github.com>
2024-07-14 16:53:43 +0200

    AddingBuilding Blocks

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,6 +1,16 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
 {
-    public class CreateProductHandler
+    public record CreateProductCommand(string Name, string Description, string ImageFile, List<string> Category, decimal Price) : IRequest<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
     {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
+
+
 }
