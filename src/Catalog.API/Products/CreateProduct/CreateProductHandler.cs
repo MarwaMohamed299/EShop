4b535dd512d9fbd0cb4f5f60305adcdbfd030f75
MarwaMohamed299 <128231159+MarwaMohamed299@users.noreply.github.com>
2024-12-17 20:24:12 +0200

    Fixing Building Blocks Error

--- src/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,6 +1,15 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
 {
-    public class CreateProductHandler
+    public record CreateProductCommmand(string Name , List<string> Category , string Description ,string ImageFile , decimal Price) : IRequest<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommmand, CreateProductResult>
     {
+        public Task<CreateProductResult> Handle(CreateProductCommmand request, CancellationToken cancellationToken)
+        {
+
+            throw new NotImplementedException();
+        }
     }
 }
