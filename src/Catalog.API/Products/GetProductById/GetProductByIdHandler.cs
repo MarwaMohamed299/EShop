ae951afe48fe3aa3be135658f1428a903d511a28
MarwaMohamed299 <128231159+MarwaMohamed299@users.noreply.github.com>
2024-12-26 11:53:23 +0200

    Adding Get Product By Id End Point

---
+++ src/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdQuery(Guid id) : IQuery<GetProductByIdResult>;
+
+public record GetProductByIdResult(Product product);
+internal class GetProductByIdHandler
+    (IDocumentSession session , ILogger<GetProductByIdResult> logger)
+    : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Get ProductByIdQueryHandler.Handle callsed with {@Query}", query);
+
+        var product = await session.LoadAsync<Product>(query.id, cancellationToken);
+
+        if(product is null)
+        {
+            throw new ProductNotFoundException();
+        }
+        return new GetProductByIdResult(product);
+    }
+}
