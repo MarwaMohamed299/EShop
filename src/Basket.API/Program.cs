5db689c950b84e0e37f47da1fa1952184d9717b7
MarwaMohamed299 <128231159+MarwaMohamed299@users.noreply.github.com>
2025-01-08 20:27:04 +0200

    Adding GetBasket + StoreBasket + DeleteBasket Endpoints

--- src/Basket.API/Program.cs
+++ src/Basket.API/Program.cs
@@ -1,12 +1,21 @@
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add Services
-
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
 // Configure Http request
+app.MapCarter();
 
 app.Run();
