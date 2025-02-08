37867598b9c96c104be1a576da40a88a3db9aeee
MarwaMohamed299 <128231159+MarwaMohamed299@users.noreply.github.com>
2025-02-09 00:39:58 +0200

    Modifing Namespaces

--- src/Ordering.Application/DependencyInjection.cs
+++ src/Ordering.Application/DependencyInjection.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 
-
 namespace Ordering.Application
 {
     public static class DependencyInjection
--- src/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Ordering.Infrastructure/DependencyInjection.cs
@@ -5,7 +5,7 @@ namespace Ordering.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services , IConfiguration configuration)
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
             var connectionString = configuration.GetConnectionString("DataBase");
 
