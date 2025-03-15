bb51815d7aef3d4e6daceed46b62d2bdfb96b0df
MarwaMohamed299 <128231159+MarwaMohamed299@users.noreply.github.com>
2025-03-15 02:43:12 +0200

    Adding Yarp in doker + adding rate limiting

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -6,19 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
-//builder.Services.AddRateLimiter(rateLimiterOptions =>
-//{
-//    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
-//    {
-//        options.Window = TimeSpan.FromSeconds(10);
-//        options.PermitLimit = 5;
-//    });
-//});
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-///app.UseRateLimiter();
+app.UseRateLimiter();
 
 app.MapReverseProxy();
 
